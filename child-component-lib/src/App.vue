52bb718714d468f1947fcceaf3d79284266d4b75
Thomas Holst <2027794+tholst@users.noreply.github.com>
2020-11-14 21:56:58 +0100

    created second package for component lib

---
+++ child-component-lib/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  components: {},
+  setup(_, { root }) {
+    console.log(root);
+  }
+});
+</script>
+
+<style lang="scss"></style>
